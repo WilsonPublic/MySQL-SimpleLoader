96e1f6c9613250667c59a91c50a5de266750f56f
WilsonPublic <38295655+WilsonPublic@users.noreply.github.com>
2018-04-28 00:17:31 +0100

    Add files via upload

---
+++ GitHub SimpleLoader/Form1.cs
@@ -0,0 +1,165 @@
+﻿//SimpleLoader by Wilson, https://github.com/WilsonPublic/SimpleLoader [Open Source Cheat Loader]
+//This is very noob friendly and can easily be adapted, this has no form of protection against cracking so please come up with your own ideas on how to prevent cracking
+//I recommend using Dot Net Reactor to protect your programs
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ManualMapInjection.Injection;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conn;
+        string HWID;
+        public Form1()
+        {
+            HWID = System.Security.Principal.WindowsIdentity.GetCurrent().User.Value; //Changing the variable "HWID (String)" to the WindowsIdentity Value, you can use any other forms of HWID, you can even use MAC/IP (Not recommended)
+            string connString;
+            connString = $"SERVER=;PORT=;DATABASE=;UID=;PASSWORD=";
+            conn = new MySqlConnection(connString);
+            InitializeComponent();
+        }
+        
+
+        public bool IsLogin()
+        {
+            string trruu = "TRUE";
+            string query = $"SELECT * FROM loader WHERE hwid='{HWID}' AND active_subscription='{trruu}';";
+
+            try
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        MessageBox.Show("Logged In");
+                        reader.Close();
+                        conn.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error Logging In");
+                        reader.Close();
+                        conn.Close();
+                        return false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error Logging In");
+                    conn.Close();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+                conn.Close();
+                return false;
+            }
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Server username or password is incorrect!" + ex.Number);
+                return false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HWID = System.Security.Principal.WindowsIdentity.GetCurrent().User.Value; //Changing the variable "HWID (String)" to the WindowsIdentity Value, you can use any other forms of HWID, you can even use MAC/IP (Not recommended)
+            textBox1.Text = HWID;
+            textBox1.ReadOnly = true;
+            checkonline();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkonline();
+            WebClient wb = new WebClient();
+            if (IsLogin()) 
+            {
+                string mainpath = "C:\\Windows\\random.dll"; //You can change the path to wherever you want but just remember to use "\\" instead of just one "\"
+                wb.DownloadFile("DLL URL", mainpath); //Replace "DLL URL" with the URL to directly download your DLL [Example: http://myurl.com/MYDLL.dll]
+                var name = "csgo"; //Replace "csgo" with any exe you want [Example: For Team Fortress 2 you would replace it with "hl2"]
+                var target = Process.GetProcessesByName(name).FirstOrDefault();
+                var path = mainpath;
+                var file = File.ReadAllBytes(path);
+
+                //Checking if the DLL isn't found
+                if (!File.Exists(path))
+                {
+                    MessageBox.Show("Error: DLL not found");
+                    return;
+                }
+                
+                //Injection, just leave this alone if you are a beginner
+                var injector = new ManualMapInjector(target) { AsyncInjection = true };
+                label2.Text = $"hmodule = 0x{injector.Inject(file).ToInt64():x8}";
+                
+                if (System.IO.File.Exists(mainpath)) //Checking if the DLL exists
+                {
+                    System.IO.File.Delete(mainpath); //Deleting the DLL
+                }
+            }
+            else
+            {
+                MessageBox.Show("HWID Incorrect");
+            }
+        }
+
+        private void checkonline()
+        {
+            //Checking if the user can get a response from "https://google.com/"
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (client.OpenRead("https://google.com/"))
+                    {
+                        label1.ForeColor = Color.Green;
+                        label1.Text = ("Online");
+                    }
+                }
+            }
+            catch
+            {
+                //If it does not get a response (This means the user is offline or google is down for some reason) it will Exit the application, you can stop this by removing "Application.Exit();"
+                label1.ForeColor = Color.Red;
+                label1.Text = ("Offline");
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(HWID);
+            button2.Enabled = false;
+            button2.Text = "Copied";
+        }
+    }
+}
